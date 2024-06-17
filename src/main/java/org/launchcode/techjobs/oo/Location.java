d46709b0a61fcc21b4835e9aab395144bf22397c
kcpage816 <kierstendotpaige@gmail.com>
2024-06-16 21:01:35 -0500

    TestTaskFive failing due to difference in line separators. although a Line Feed (LF) character is used, the comparison failure window shows it as a Carriage Return and Line Feed Character

--- src/main/java/org/launchcode/techjobs/oo/Location.java
+++ src/main/java/org/launchcode/techjobs/oo/Location.java
@@ -4,8 +4,27 @@ import java.util.Objects;
 
 public class Location extends JobField{
 
+    // TODO: Add a constructor that takes a string as a parameter and assigns it to the 'value' field. The
+    //  constructor should also call the empty constructor in order to initialize the 'id' field.
     public Location(String value) {
         super(value);
     }
+
+    // Custom toString, equals, and hashCode methods:
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Location)) return false;
+        Location location = (Location) o;
+        return getId() == location.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    // Getters and Setters:
 
 }
--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -1,11 +1,18 @@
 package org.launchcode.techjobs.oo;
 
-import java.util.Objects;
+public class PositionType extends JobField{
 
-public class PositionType extends JobField {
 
     public PositionType(String value) {
         super(value);
     }
+
+    // TODO: Add a custom toString() method that returns the data stored in 'value'.
+
+
+    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    //  their id fields match.
+
+    // Getters and Setters:
 
 }
